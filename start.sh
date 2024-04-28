f44c32815d17f5dad4ac488be7c2198e2b5cafc5
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2024-04-28 23:30:09 +0000

    Change script call sequence

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ set -euxo pipefail
 
 (cd kind && bash -c ./start.sh)
 (cd victoriametrics/operator && bash -c ./apply.sh)
-(cd victoriametrics/manifest && bash -c ./apply.sh)
 (cd victoriametrics/config && bash -c ./apply.sh)
+(cd victoriametrics/manifest && bash -c ./apply.sh)
 (cd tools/manifest && bash -c ./apply.sh)
 (cd prometheus/manifest && bash -c ./apply.sh)
 (cd grafana/manifest && bash -c ./apply.sh)
--- stop.sh
+++ stop.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 (cd grafana/manifest && bash -c ./delete.sh)
 (cd prometheus/manifest && bash -c ./delete.sh)
 (cd tools/manifest && bash -c ./apply.sh)
-(cd victoriametrics/config && bash -c ./delete.sh)
 (cd victoriametrics/manifest && bash -c ./delete.sh)
+(cd victoriametrics/config && bash -c ./delete.sh)
 (cd victoriametrics/operator && bash -c ./delete.sh)
 (cd kind && bash -c ./stop.sh)
 
