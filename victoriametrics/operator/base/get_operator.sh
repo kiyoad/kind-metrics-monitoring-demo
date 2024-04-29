bb0491be74975d10ed71145ea2a325fd87323a48
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2024-04-29 01:54:10 +0000

    Use newest operator

--- victoriametrics/operator/base/get_operator.sh
+++ victoriametrics/operator/base/get_operator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
-curl -fsSLO  https://github.com/VictoriaMetrics/operator/releases/download/v0.42.4/bundle_crd.zip
+curl -fsSLO  https://github.com/VictoriaMetrics/operator/releases/download/v0.43.5/bundle_crd.zip
 rm -rf ./release
 unzip bundle_crd.zip
