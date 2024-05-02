7616c5077be32ca672341a31875e679f8fd436b7
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2024-05-02 08:52:08 +0000

    Use Calico for use NetworkPolicy https://docs.tigera.io/calico/latest/getting-started/kubernetes/kind

--- kind/start.sh
+++ kind/start.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 set -euxo pipefail
 sed "s|%STORAGE_PATH%|$(pwd)|" cluster_tmpl.yaml | kind create cluster --config -
+
+while kubectl get pods --no-headers -A | grep -v Pending | grep -v Running > /dev/null
+do
+  watch -g kubectl get pods -A
+done
+
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/calico.yaml
+
+while kubectl get pods --no-headers -A | grep -v Running > /dev/null
+do
+  watch -g kubectl get pods -A
+done
+
 kubectl wait -A --for=condition=Ready pod --all
 kubectl apply -f pv.yaml
 
