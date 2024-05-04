b23e0ad8521cf2c4af98d8c810c51d9268fb964e
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2024-05-04 11:48:00 +0000

    Call get_operator before start operator

--- victoriametrics/operator/apply.sh
+++ victoriametrics/operator/apply.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
+if ! [[ -d base/release ]]; then
+    (cd base && bash -c ./get_operator.sh)
+fi
 kubectl apply -f base/release/crds
 kubectl apply --kustomize=overlays
 kubectl wait -A --for=condition=Ready pod --all
