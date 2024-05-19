a019ef66b9b3e9856a4f1692841f580fdafd3310
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2024-05-19 16:49:45 +0900

    Use default scrape_configs: in helm/prometheus

--- grafana/helm/grafana-tmpl-patcher.sh
+++ grafana/helm/grafana-tmpl-patcher.sh
@@ -7,5 +7,4 @@ function rm_tmpfile {
 trap rm_tmpfile EXIT
 trap 'trap - EXIT; rm_tmpfile; exit -1' INT PIPE TERM
 NAMESPACE=$(tee ${tmpfile} | sed -ne "s/ *namespace: *\(.*\)/\1/p" | head -1)
-sed -e "s/\.default\./\.${NAMESPACE}\./" -e "s/regex: default/regex: ${NAMESPACE}/" ${tmpfile}
-
+sed -e "s/\.default\.svc\.cluster\.local:/\.${NAMESPACE}\.svc\.cluster\.local:/" ${tmpfile}
