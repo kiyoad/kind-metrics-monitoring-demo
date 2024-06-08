0afed3d35f860475d89157ca86a681b7791acea8
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2024-06-08 13:37:17 +0900

    Delete the test Pod after grafana starts

--- grafana/helm/install.sh
+++ grafana/helm/install.sh
@@ -3,3 +3,6 @@ set -euxo pipefail
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 helm template --namespace monitoring demo grafana/grafana -f grafana-values.yaml | ./grafana-tmpl-patcher.sh | kubectl apply -f -
+sleep 5
+kubectl delete pod -n monitoring demo-grafana-test || /usr/bin/true
+
