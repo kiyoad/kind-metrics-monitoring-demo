ebeea5d579d8f1fe3ab7d0373aabcaa55f4d4923
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2024-06-10 14:42:27 +0900

    Remove prometheus helm version

--- prometheus/helm/uninstall.sh
+++ prometheus/helm/uninstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -euxo pipefail
-helm template --namespace monitoring demo prometheus-community/prometheus -f prometheus-values.yaml --version 23.1.0 | ./prometheus-tmpl-patcher.sh | kubectl delete -f - || /usr/bin/true
+helm template --namespace monitoring demo prometheus-community/prometheus -f prometheus-values.yaml | ./prometheus-tmpl-patcher.sh | kubectl delete -f - || /usr/bin/true
