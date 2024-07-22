655730b67dfbb7f2709728296fcdfcbfe32d3274
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2024-07-22 13:39:49 +0900

    Change namespace support

--- tools/manifest/apply.sh
+++ tools/manifest/apply.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
+
+cat << EOF > overlays/kustomization.yaml
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+  - ../base
+namespace: $(../../namespace/get.sh)
+EOF
+
 kubectl apply --kustomize=overlays
 
