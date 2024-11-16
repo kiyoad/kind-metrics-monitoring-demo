80f9f19b2468fdd72a8a6688bc071091c04e637b
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2024-11-16 19:22:10 +0900

    Remove unused grafana-tmpl-patcher.sh

--- grafana/helm/install.sh
+++ grafana/helm/install.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-helm template --namespace $(../../namespace/get.sh) demo grafana/grafana -f grafana-values.yaml | ./grafana-tmpl-patcher.sh | kubectl apply -f -
+helm template --namespace $(../../namespace/get.sh) demo grafana/grafana -f grafana-values.yaml | kubectl apply -f -
 sleep 5
 kubectl delete pod -n $(../../namespace/get.sh) demo-grafana-test || true
 
