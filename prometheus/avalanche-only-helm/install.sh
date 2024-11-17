68c281186d0a43095bebef9b522f021cda020968
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2024-11-17 16:18:05 +0900

    Remove unused prometheus-tmpl-patcher.sh

--- prometheus/avalanche-only-helm/install.sh
+++ prometheus/avalanche-only-helm/install.sh
@@ -2,5 +2,5 @@
 set -euxo pipefail
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm template --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus -f prometheus-values.yaml | ./prometheus-tmpl-patcher.sh | kubectl apply -f -
+helm template --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus -f prometheus-values.yaml | kubectl apply -f -
 
--- prometheus/avalanche-only-helm/uninstall.sh
+++ prometheus/avalanche-only-helm/uninstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -euxo pipefail
-helm template --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus -f prometheus-values.yaml | ./prometheus-tmpl-patcher.sh | kubectl delete -f - || true
+helm template --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus -f prometheus-values.yaml | kubectl delete -f - || true
