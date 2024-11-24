6018f4c4966a91ced32e1a4b4f0c71e1d5c8df62
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2024-11-24 20:11:13 +0900

    Add local registry for kind

--- kind/start.sh
+++ kind/start.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 set -euxo pipefail
+
+# Create registry container unless it already exists
+reg_name='kind-registry'
+reg_port='5001'
+if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --network bridge --name "${reg_name}" \
+    registry:2
+fi
+
 sed "s|%STORAGE_PATH%|$(pwd)|" cluster_tmpl.yaml | kind create cluster --config -
+
+# Add the registry config to the nodes
+REGISTRY_DIR="/etc/containerd/certs.d/localhost:${reg_port}"
+for node in $(kind get nodes); do
+  docker exec "${node}" mkdir -p "${REGISTRY_DIR}"
+  cat <<EOF | docker exec -i "${node}" cp /dev/stdin "${REGISTRY_DIR}/hosts.toml"
+[host."http://${reg_name}:5000"]
+EOF
+done
+
+# Connect the registry to the cluster network if not already connected
+if [ "$(docker inspect -f='{{json .NetworkSettings.Networks.kind}}' "${reg_name}")" = 'null' ]; then
+  docker network connect "kind" "${reg_name}"
+fi
+
+# Document the local registry
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: local-registry-hosting
+  namespace: kube-public
+data:
+  localRegistryHosting.v1: |
+    host: "localhost:${reg_port}"
+    help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
+EOF
 
 while kubectl get pods --no-headers -A | grep -v Pending | grep -v Running > /dev/null
 do
--- kind/stop.sh
+++ kind/stop.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 kind delete cluster
+docker stop kind-registry
+docker rm kind-registry
 
