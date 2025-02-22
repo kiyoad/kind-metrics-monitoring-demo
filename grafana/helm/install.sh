33e5eef5a28b0faa80aa54a8e776dd9bd3686c83
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2025-02-22 21:35:44 +0900

    Remove demo-grafana-test

--- grafana/helm/install.sh
+++ grafana/helm/install.sh
@@ -3,6 +3,3 @@ set -euxo pipefail
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 helm template --namespace $(../../namespace/get.sh) demo grafana/grafana -f grafana-values.yaml | kubectl apply -f -
-sleep 5
-kubectl delete pod -n $(../../namespace/get.sh) demo-grafana-test || true
-
