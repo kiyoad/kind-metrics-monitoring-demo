7e7a03ec64b5a13bac49d5349e113981f1e9c026
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2025-02-24 13:33:32 +0900

    Use persistent volume in pushgateway

--- prometheus-pushgateway/helm/uninstall.sh
+++ prometheus-pushgateway/helm/uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 helm template --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus-pushgateway -f config.yaml | kubectl delete -f - || true
-
+kubectl patch pv worker-pv35 -p '{"spec":{"claimRef": null}}'
