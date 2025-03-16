6bdd4dcbe8b58a1eccf4850f0e47d04e95e04dea
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2025-03-17 07:05:16 +0900

    Update fluent-bit,victoriametrics, and more.

--- victoriametrics/vlogs/cli/vl.sh
+++ victoriametrics/vlogs/cli/vl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -euo pipefail
-docker run --rm -it --network host victoriametrics/vlogscli:v1.10.1-victorialogs -datasource.url='http://localhost:9428/select/logsql/query'
+docker run --rm -it --network host victoriametrics/vlogscli:v1.17.0-victorialogs -datasource.url='http://localhost:9428/select/logsql/query'
