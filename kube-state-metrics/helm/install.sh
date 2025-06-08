88425239d878377fdb95635832b02068241e834e
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2025-06-08 21:54:01 +0900

    Use helm version info for kube-state-metrics

--- kube-state-metrics/helm/install.sh
+++ kube-state-metrics/helm/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
-helm template --namespace $(../../namespace/get.sh) demo prometheus-community/kube-state-metrics -f config.yaml | kubectl apply -f -
+helm show values --version $(../../kind/get-chart-ver.sh kube-state-metrics/kube-state-metrics) --namespace $(../../namespace/get.sh) prometheus-community/kube-state-metrics >values.yaml
+helm template --version $(../../kind/get-chart-ver.sh kube-state-metrics/kube-state-metrics) --namespace $(../../namespace/get.sh) demo prometheus-community/kube-state-metrics -f config.yaml | kubectl apply -f -
--- kube-state-metrics/helm/uninstall.sh
+++ kube-state-metrics/helm/uninstall.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -euxo pipefail
-helm template --namespace $(../../namespace/get.sh) demo prometheus-community/kube-state-metrics -f config.yaml | kubectl delete -f - || true
-
+helm template --version $(../../kind/get-chart-ver.sh kube-state-metrics/kube-state-metrics) --namespace $(../../namespace/get.sh) demo prometheus-community/kube-state-metrics -f config.yaml | kubectl delete -f - || true
