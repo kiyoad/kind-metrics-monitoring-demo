6ab7cd2206cf5b01b5962a567df1da639a5bfc46
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2025-06-08 21:57:11 +0900

    Use helm version info for pushgateway

--- prometheus-pushgateway/helm/install.sh
+++ prometheus-pushgateway/helm/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
-helm template --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus-pushgateway -f config.yaml | kubectl apply -f -
+helm show values --version $(../../kind/get-chart-ver.sh prometheus/pushgateway) --namespace $(../../namespace/get.sh) prometheus-community/prometheus-pushgateway >values.yaml
+helm template --version $(../../kind/get-chart-ver.sh prometheus/pushgateway) --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus-pushgateway -f config.yaml | kubectl apply -f -
--- prometheus-pushgateway/helm/uninstall.sh
+++ prometheus-pushgateway/helm/uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
-helm template --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus-pushgateway -f config.yaml | kubectl delete -f - || true
+helm template --version $(../../kind/get-chart-ver.sh prometheus/pushgateway) --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus-pushgateway -f config.yaml | kubectl delete -f - || true
 kubectl patch pv worker-pv35 -p '{"spec":{"claimRef": null}}'
