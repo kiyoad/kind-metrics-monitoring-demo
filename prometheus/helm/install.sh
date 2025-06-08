05bce5b2b1b2960a5dbd80f6e8fe5554403957ad
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2025-06-08 21:54:45 +0900

    Use helm version info for prometheus

--- prometheus/helm/install.sh
+++ prometheus/helm/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
+helm show values --version $(../../kind/get-chart-ver.sh prometheus/prometheus) --namespace $(../../namespace/get.sh) prometheus-community/prometheus >values.yaml
 cat ./prometheus-config-part.yaml ./serverFiles/$1.yaml >prometheus-config.yaml
-helm template --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus -f ./prometheus-config.yaml | kubectl apply -f -
+helm template --version $(../../kind/get-chart-ver.sh prometheus/prometheus) --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus -f ./prometheus-config.yaml | kubectl apply -f -
 rm -f ./prometheus-config.yaml
--- prometheus/helm/uninstall.sh
+++ prometheus/helm/uninstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 cat ./prometheus-config-part.yaml ./serverFiles/$1.yaml >prometheus-config.yaml
-helm template --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus -f ./prometheus-config.yaml | kubectl delete -f - || true
+helm template --version $(../../kind/get-chart-ver.sh prometheus/prometheus) --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus -f ./prometheus-config.yaml | kubectl delete -f - || true
 rm -f ./prometheus-config.yaml
