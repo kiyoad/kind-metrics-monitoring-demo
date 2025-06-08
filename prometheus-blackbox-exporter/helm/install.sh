b90cfa7a74431e5acdb123585cb74f55f24c63bc
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2025-06-08 21:55:47 +0900

    Use helm version info for blackbox-exporter

--- prometheus-blackbox-exporter/helm/install.sh
+++ prometheus-blackbox-exporter/helm/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
-helm template --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus-blackbox-exporter -f config.yaml | kubectl apply -f -
+helm show values --version $(../../kind/get-chart-ver.sh prometheus/blackbox-exporter) --namespace $(../../namespace/get.sh) prometheus-community/prometheus-blackbox-exporter >values.yaml
+helm template --version $(../../kind/get-chart-ver.sh prometheus/blackbox-exporter) --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus-blackbox-exporter -f config.yaml | kubectl apply -f -
--- prometheus-blackbox-exporter/helm/uninstall.sh
+++ prometheus-blackbox-exporter/helm/uninstall.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -euxo pipefail
-helm template --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus-blackbox-exporter -f config.yaml | kubectl delete -f - || true
-
+helm template --version $(../../kind/get-chart-ver.sh prometheus/blackbox-exporter) --namespace $(../../namespace/get.sh) demo prometheus-community/prometheus-blackbox-exporter -f config.yaml | kubectl delete -f - || true
