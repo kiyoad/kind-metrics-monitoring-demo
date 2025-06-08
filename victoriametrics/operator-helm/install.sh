c364889d1d8cdc3b5acf7edd276027935a93a11a
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2025-06-08 21:58:33 +0900

    Use helm version info for VMoperator

--- victoriametrics/operator-helm/install.sh
+++ victoriametrics/operator-helm/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
-helm template --namespace $(../../namespace/get.sh) demo vm/victoria-metrics-operator -f config.yaml | kubectl apply -f -
+helm show values --version $(../../kind/get-chart-ver.sh victoriametrics/operator) --namespace $(../../namespace/get.sh) vm/victoria-metrics-operator >values.yaml
+helm template --version $(../../kind/get-chart-ver.sh victoriametrics/operator) --namespace $(../../namespace/get.sh) demo vm/victoria-metrics-operator -f config.yaml | kubectl apply -f -
 kubectl wait -A --for=condition=Ready pod --all
--- victoriametrics/operator-helm/uninstall.sh
+++ victoriametrics/operator-helm/uninstall.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -euxo pipefail
-helm template --namespace $(../../namespace/get.sh) demo vm/victoria-metrics-operator -f config.yaml | kubectl delete -f - || true
-
+helm template --version $(../../kind/get-chart-ver.sh victoriametrics/operator) --namespace $(../../namespace/get.sh) demo vm/victoria-metrics-operator -f config.yaml | kubectl delete -f - || true
